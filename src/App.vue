06e9f8ee8b4d094fd7d0a771b9f4d93d8bbd3ffc
cbgduc <ducnguyenvlg@gmail.com>
2024-11-06 10:47:54 +0700

    intial commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div> <router-view /></div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  components:{
+
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Arial, Helvetica, sans-serif;
+  -webkit-font-smoothing: antialised;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
